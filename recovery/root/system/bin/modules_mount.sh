55011c3adad81cd3bdccadb5edc2b087b2cd04ee
MisterZtr <andey2004586@gmail.com>
2024-04-06 13:18:53 +0300

    modules: Use different directories for mount modules

--- recovery/root/system/bin/modules_mount.sh
+++ recovery/root/system/bin/modules_mount.sh
@@ -2,10 +2,10 @@
 if [ -f "/data/adb/ksu/modules.img" ]; then
   e2fsck -fy /data/adb/ksu/modules.img;
   resize2fs -M /data/adb/ksu/modules.img;
-  mount -t auto -o loop /data/adb/ksu/modules.img /data/adb/modules;
+  mount -t auto -o loop /data/adb/ksu/modules.img /data/adb/ksu-modules;
 fi;
 
 if [ -f "/data/adb/ap/modules.img" ]; then
   e2fsck -fy /data/adb/ap/modules.img;
-  mount -t auto -o loop /data/adb/ap/modules.img /data/adb/modules;
+  mount -t auto -o loop /data/adb/ap/modules.img /data/adb/ap-modules;
 fi;
